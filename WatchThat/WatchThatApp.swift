4f86f7b050d6ca3621a1d77794c1607b8b27265f
Igor Camilo <11697241+igorcamilo@users.noreply.github.com>
2024-04-09 17:18:26 +0200

    Added Xcode project

---
+++ WatchThat/WatchThatApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchThatApp.swift
+//  WatchThat
+//
+//  Created by Igor Camilo on 09.04.24.
+//
+
+import SwiftUI
+
+@main
+struct WatchThatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
